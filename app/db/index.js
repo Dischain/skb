bf00a609c8b1a454d93fa9459ced364f59efb9a0
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-06-07 03:44:03 +0300

    from initial commit

---
+++ app/db/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Mongoose = require('mongoose');
+
+const config = require('../config');
+
+var dbURI = "mongodb://" + 
+						encodeURIComponent(config.db.username) + ":" + 
+						encodeURIComponent(config.db.password) + "@" + 
+						config.db.host + ":" + 
+						config.db.port + "/" + 
+						config.db.name;
+
+Mongoose.connect(dbURI);
+
+Mongoose.connection.on('error', function(err) {
+	if(err) throw err;
+});
+
+Mongoose.Promise = global.Promise;
+
+module.exports = {  
+	models: {
+		UserModel: require('./schemas/user.js'),
+		CategoryModel: require('./schemas/category.js'),
+		ArticleModel: require('./schemas/article.js')
+	}
+};
