1e348ae6d8f78f751338619c41ba9c1c4b49a38a
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-06-19 22:32:01 +0300

    add utils

---
+++ app/models/util/index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const UserModel = require('../../db').models.UserModel;
+const CategoryModel = require('../../db').models.CategoryModel;
+const ArticleModel = require('../../db').models.ArticleModel;
+
+/*Users*/
+exports.deleteAllUsers = function() {
+  return UserModel.remove({});
+}
+
+/*Categories*/
+exports.deleteAllCategories = function() {
+  return CategoryModel.remove({});
+}
+
+exports.deleteAllArticles = function() {
+  return ArticleModel.remove({});
+}
+
+/*
+ * Replaces the current name of category in the path to it with new name
+ *
+ * Example:
+ *	if path to category is 'path/category/oldname/'
+ *	end new name of category is 'some_new_name'
+ *	then it returns 'path/category/some_new_name/'
+ */
+exports.replaceCategoryNameAtPath = function(path, name) {
+	let newPath = exports.getCategoryParentPath(path) + name + '/';
+
+	return newPath;
+}
+
+exports.replaceArticleNameAtPath = function(path, name) {
+	console.log('exports.getArticleParentPath(path): ' + 
+		exports.getArticleParentPath(path));
+	let newPath = exports.getArticleParentPath(path) + name;
+	return newPath;
+}
+
+/*
+ * Returns path which should be replaced in oreder to remove category
+ *
+ * Example:
+ *	path = 'user1/programming/databases/mongodb/'
+ *  getPathToReplace(path) // -> outputs 'user1/programming/databases/''
+ */
+exports.getCategoryParentPath = function(path) {
+	let pathWithRemovedLastSlash = path.slice(0, path.length - 1);
+	let index = pathWithRemovedLastSlash.lastIndexOf('/');
+
+	let parentPath = pathWithRemovedLastSlash.slice(0, index + 1);
+
+	return parentPath;
+}
+
+exports.getArticleParentPath = function(path) {
+	let index = path.lastIndexOf('/');
+	let articlePath = path.slice(0, index + 1);
+
+	return articlePath;
+}
+
+exports.getCategoryNameByPath = function(path) {
+	let pathWithRemovedLastSlash = path.slice(0, path.length - 1);
+	let index = pathWithRemovedLastSlash.lastIndexOf('/');
+
+	let categoryName = pathWithRemovedLastSlash.slice(index + 1);
+
+	return categoryName;
+}
+
+exports.getArticleNameByPath = function(path) {
+	let index = path.lastIndexOf('/');
+	let articleName = path.slice(index + 1);
+
+	return articleName;
+}
+
+exports.getOwnerName = function(path) {
+	let index = path.indexOf('/');
+	let ownerName = path.slice(0, index);
+
+	return ownerName;
+}
---
+++ app/routes/rout_util.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const SP = ' ';
+const DIVISOR = '+'
+
+/**
+ * Reduce and trim spaces at string.
+ *
+ * Example: let str = '  hello    world  ';
+ * reduceSpaces(str); // -> 'hello world'
+ */
+function trimSpaces(str) {
+	let trimedStrig = '';
+	let temp = str.trim();
+
+	for (let i = 0; i < temp.length; i++) {
+		if (temp[i] === SP) {
+			if (temp[i - 1] === SP && i > 0) {
+				continue;
+			} else {
+				trimedStrig += SP;
+			}
+		} else {
+			trimedStrig += temp[i];
+		}
+	}
+
+  return trimedStrig;
+}
+
+/**
+ * Convert spaces to specified divisor symbol
+ *
+ * Example: let str = 'hello world';
+ * convertSpacesToDivisor(str); // -> 'hell+world'
+ */
+ function convertSpacesToDivisor(str) {
+  let convertedString = '';
+
+  for (let i = 0; i < str.length; i++) {
+    convertedString += str[i] === SP ? DIVISOR : str[i];
+  }
+
+  return convertedString;
+ }
