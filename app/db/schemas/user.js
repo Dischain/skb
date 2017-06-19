4f9bd36855f8497ea0818ccba3b2ac65e167aa10
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-06-19 22:18:36 +0300

    replaced async validatePass with promise

--- app/db/schemas/user.js
+++ app/db/schemas/user.js
@@ -36,13 +36,14 @@ UserSchema.pre('save', function(next) {
   });
 });
 
-UserSchema.methods.validatePassword = function(password, callback) {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
-      if (err) 
-      	return callback(err);
-      callback(null, isMatch);
+UserSchema.methods.validatePassword = function(password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+      if (err) reject(err);
+      resolve(isMatch);
+    });
   });
-};
+}
 
 var UserModel = Mongoose.model('user', UserSchema);
 
