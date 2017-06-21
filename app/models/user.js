0b29b7b3b0dc75dd7a13de3e5eb0e20e1f40886d
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-06-21 21:10:35 +0300

    updated rout handler

--- app/models/user.js
+++ app/models/user.js
@@ -2,45 +2,73 @@
 
 const UserModel = require('../db').models.UserModel;
 const CategoryModel = require('../db').models.CategoryModel;
+const Categories = require('./category.js');
 
 /*
  * Creates user by specified data fields
  *
  * Example:
- *	create({
- *		username: 'some_name',
- *		password: 'pas',
- *		email: 'mail@ya.ru',
- *		socialId: '111',
- *		avatar: '...'
- *	}).then(() => { do_soeth(); });
+ *  create({
+ *    username: 'some_name',
+ *    password: 'pas',
+ *    email: 'mail@ya.ru',
+ *    socialId: '111',
+ *    avatar: '...'
+ *  }).then(() => { do_soeth(); });
  */
 exports.create = function(userData, cb) {
-	let user = new UserModel(userData);
-	
-	let dafaultRootCategory = new CategoryModel({ 
-		name: '/',
-		path: user.username + '/',
-		_parent: null
-	})
+  let user = new UserModel(userData);
+  
+  let dafaultRootCategory = new CategoryModel({ 
+    name: '/',
+    path: user.username + '/',
+    _parent: null
+  })
 
-	user._root = dafaultRootCategory;
+  user._root = dafaultRootCategory;
 
-	return user.save()
-		.then(() => dafaultRootCategory.save() )
-		//.then(() => cb())
+  return user.save()
+    .then(() => dafaultRootCategory.save() )
+    //.then(() => cb())
 }
 
 /*
  * Finds user by specified data fields
  *
  * Example:
- * 		findOne({ name: 'somename' }).then(() => { do_soeth(); });
+ *    findOne({ name: 'somename' }).then(() => { do_soeth(); });
  */
 exports.findOne = function(data) {
-	return UserModel.findOne(data);
+  return UserModel.findOne(data);
+}
+
+/*
+ * Returns user entry with user data and handy statistics
+ */
+exports.getUserEntry = function(data) {
+  let tempUser;
+
+  return exports.findOne(data)
+    .populate('_root')
+    .then((user) => {
+      tempUser = user;
+
+      return Categories.getSubcategories(user._root.path)
+        .then((subcategories) => {
+          let result = {};
+          result.user = tempUser;
+          result.subcats = subcategories;
+
+          return result;
+        });
+    })
+    /*.then((result) => {
+      let statistics_p = result.subcats.categories.map((cat) => {
+        return Categories.countSubcategories(cat.path);
+      })
+    })*/
 }
 
 exports.findAll = function() {
-	return UserModel.find({});
+  return UserModel.find({});
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -7,8 +7,8 @@ const Users = require('../models/user.js');
 
 //TODO: handle auth
 router.get('/:username', (req, res) => {
-  Users.findOne({ username: req.params.username })
-    .then((user) => res.json(user))
+  Users.getUserEntry({ username: req.params.username })
+    .then((user) => { console.log(user); res.json(user) })
     .catch((err) => {
       res.status(412).json({msg: error.message});
     });
